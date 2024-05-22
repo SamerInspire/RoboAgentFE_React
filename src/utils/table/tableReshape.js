11d3e18a4db938852e66c9a362e82b8ebf2b79fd
SamerInspire <samer.ibrahim@inspirejo.com>
2024-05-22 11:19:22 +0300

    fix users table & get answer & table reshape enhance

--- src/utils/table/tableReshape.js
+++ src/utils/table/tableReshape.js
@@ -10,13 +10,9 @@ export function handleFilterServices(activeServices = [], allServices = []) {
 }
 export function reshapeUserData(usersArr = []) {
   const newUsersArr = [];
-  for (let user of usersArr) {
-    let newUserArr = [];
-    for (let key of Object.keys(user)) {
-      newUserArr.push(user[key]);
-    }
-
-    newUsersArr.push(newUserArr);
-  }
+  usersArr.forEach(user=>{
+    user.status = user.status.split('-')[1]
+    newUsersArr.push(Object.values(user))
+  })
   return newUsersArr;
 }
