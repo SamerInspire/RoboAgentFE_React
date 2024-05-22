b18db4a00eb4d112c6807f942dc7d94e39c37274
Armada6199 <moh.abdin27@gmail.com>
2024-05-22 11:31:06 +0300

    70 percent  coverage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import { useTranslation } from "react-i18next";
 import "./App.css";
 import Providers from "./components/Providers";
 import Routes from "./routes";
 const App = () => {
-  const { i18n } = useTranslation();
-  //
   return (
     <Providers>
       <Routes />
