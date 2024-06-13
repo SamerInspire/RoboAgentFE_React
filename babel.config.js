f5fe2bb9d29ab37d0fa1ef7038c3bf77ae86717f
SamerInspire <samer.ibrahim@inspirejo.com>
2024-06-13 12:44:10 +0300

    adding

--- babel.config.js
+++ babel.config.js
@@ -1,1 +1,11 @@
-module.exports = { presets: ["@babel/preset-env"] };
+// module.exports = { presets: ["@babel/preset-env"] };
+module.exports = {
+    presets: [
+      ['@babel/preset-env', { targets: 'defaults' }],
+      '@babel/preset-typescript',
+      ['@babel/preset-react', { runtime: 'automatic', development: process.env.NODE_ENV !== 'production' }],
+    ],
+    plugins: ['@babel/plugin-transform-runtime', 'babel-plugin-styled-components'],
+  };
+  
+  
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: './src/index.js',
+    module: {
+      rules: [
+        //...
+        {
+          test: /\.(png|jp(e*)g|svg|gif)$/,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    //...
+  };
+  
+  
