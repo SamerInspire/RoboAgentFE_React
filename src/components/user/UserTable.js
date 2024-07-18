e8bfe7aae168328ccc0cbd287d12aaf37737bfd8
Armada6199 <moh.abdin27@gmail.com>
2024-07-18 10:31:01 +0300

    fixing table update

--- src/components/user/UserTable.js
+++ src/components/user/UserTable.js
@@ -147,170 +147,162 @@ function UserTable() {
         <MUIDataTable
           title={t('usersTable.Users List')}
           data={tableData}
-          columns={
-            isLoading
-              ? []
-              : [
-                  {
-                    name: 'userId',
-                    label: 'User ID',
-                    options: {
-                      filter: true,
-                      display: 'none',
-                      customBodyRender: (value, tableMeta, updateValue) => {
-                        return <Grid display={'none'}></Grid>;
-                      },
-                    },
-                  },
-                  {
-                    name: 'UserName',
-                    label: t('UserNameLabel'),
-                    options: {
-                      filter: true,
-                    },
-                  },
-                  {
-                    name: 'FirstName',
-                    label: t('firstnameLabel'),
-                    options: {
-                      display: 'none',
-                      filter: false,
-                    },
-                  },
-                  {
-                    name: 'middleName',
-                    label: t('middlenameLabel'),
+          columns={[
+            {
+              name: 'userId',
+              label: 'User ID',
+              options: {
+                filter: true,
+                display: 'none',
+                customBodyRender: (value, tableMeta, updateValue) => {
+                  return <Grid display={'none'}></Grid>;
+                },
+              },
+            },
+            {
+              name: 'UserName',
+              label: t('UserNameLabel'),
+              options: {
+                filter: true,
+              },
+            },
+            {
+              name: 'FirstName',
+              label: t('firstnameLabel'),
+              options: {
+                display: 'none',
+                filter: false,
+              },
+            },
+            {
+              name: 'middleName',
+              label: t('middlenameLabel'),
 
-                    options: {
-                      display: 'none',
+              options: {
+                display: 'none',
 
-                      filter: false,
-                    },
-                  },
-                  {
-                    name: 'last Name',
-                    label: t('lastnameLabel'),
+                filter: false,
+              },
+            },
+            {
+              name: 'last Name',
+              label: t('lastnameLabel'),
 
-                    options: {
-                      filter: false,
-                      display: 'none',
-                    },
-                  },
-                  {
-                    name: 'status',
-                    label: t('statusLabel'),
+              options: {
+                filter: false,
+                display: 'none',
+              },
+            },
+            {
+              name: 'status',
+              label: t('statusLabel'),
 
-                    options: {
-                      filter: true,
-                    },
-                  },
-                  {
-                    name: 'role',
-                    label: t('roleLabel'),
+              options: {
+                filter: true,
+              },
+            },
+            {
+              name: 'role',
+              label: t('roleLabel'),
 
-                    options: {
-                      filter: true,
-                      display: currentUserData.role == 'TEAM_LEAD' ? 'none' : true,
-                      customBodyRender: (value, tableMeta, updateValue) => {
-                        return (
-                          <Grid container item minWidth={'150px'} alignItems={'center'} gap={2}>
-                            <Grid item xs={8}>
-                              <Typography variant="body2" fontWeight={500}>
-                                {value}
-                              </Typography>
-                            </Grid>
-                            <Grid
-                              item
-                              xs={2}
-                              sx={{ cursor: 'pointer' }}
-                              onClick={(event) => handleStatusClick(event, tableMeta.rowData)}
-                            >
-                              <MoreVertIcon sx={{ color: 'primary.main' }} />
-                            </Grid>
+              options: {
+                filter: true,
+                display: currentUserData.role == 'TEAM_LEAD' ? 'none' : true,
+                customBodyRender: (value, tableMeta, updateValue) => {
+                  return (
+                    <Grid container item minWidth={'150px'} alignItems={'center'} gap={2}>
+                      <Grid item xs={8}>
+                        <Typography variant="body2" fontWeight={500}>
+                          {value}
+                        </Typography>
+                      </Grid>
+                      <Grid
+                        item
+                        xs={2}
+                        sx={{ cursor: 'pointer' }}
+                        onClick={(event) => handleStatusClick(event, tableMeta.rowData)}
+                      >
+                        <MoreVertIcon sx={{ color: 'primary.main' }} />
+                      </Grid>
+                    </Grid>
+                  );
+                },
+              },
+            },
+            {
+              name: 'email',
+              label: t('emailLabel'),
+
+              options: {
+                filter: true,
+              },
+            },
+            currentUserData.role == 'TEAM_LEAD'
+              ? {
+                  name: 'service',
+                  label: t('serviceLabel'),
+                }
+              : {
+                  name: 'service',
+                  label: t('serviceLabel'),
+                  options: {
+                    filter: true,
+                    display: currentUserData.role == 'TEAM_LEAD' ? 'none' : true,
+                    customBodyRender: (value, tableMeta, updateValue) => {
+                      const formattedValue = value.split('_').join(' ');
+                      return (
+                        <Grid container item minWidth={'200px'} maxWidth={'200px'} alignItems={'center'} gap={2}>
+                          <Grid item xs={8}>
+                            <Typography variant="body2" fontWeight={500}>
+                              {formattedValue}
+                            </Typography>
                           </Grid>
-                        );
-                      },
-                    },
-                  },
-                  {
-                    name: 'email',
-                    label: t('emailLabel'),
-
-                    options: {
-                      filter: true,
-                    },
-                  },
-                  currentUserData.role == 'TEAM_LEAD'
-                    ? {
-                        name: 'service',
-                        label: t('serviceLabel'),
-                      }
-                    : {
-                        name: 'service',
-                        label: t('serviceLabel'),
-                        options: {
-                          filter: true,
-                          display: currentUserData.role == 'TEAM_LEAD' ? 'none' : true,
-                          customBodyRender: (value, tableMeta, updateValue) => {
-                            const formattedValue = value.split('_').join(' ');
-                            return (
-                              <Grid container item minWidth={'200px'} maxWidth={'200px'} alignItems={'center'} gap={2}>
-                                <Grid item xs={8}>
-                                  <Typography variant="body2" fontWeight={500}>
-                                    {formattedValue}
-                                  </Typography>
-                                </Grid>
-                                <Grid
-                                  item
-                                  xs={2}
-                                  sx={{ cursor: 'pointer' }}
-                                  onClick={() => handleOpenServiceDialog(tableMeta.rowData)}
-                                >
-                                  <MoreVertIcon sx={{ color: 'primary.main' }} />
-                                </Grid>
-                              </Grid>
-                            );
-                          },
-                        },
-                      },
-                  {
-                    name: 'services',
-                    label: t('servicesLabel'),
-                    options: {
-                      filter: false,
-                      customBodyRender: (value, tableMeta, updateValue) => {
-                        return (
-                          <Grid container item minWidth={'150px'} maxWidth={'150px'} alignItems={'center'}>
-                            <Button
-                              fullWidth
-                              variant="contained"
-                              onClick={() => handleOpenServiceModal(tableMeta.rowData)}
-                            >
-                              {t('usersTable.VIEW ALL')}
-                            </Button>
+                          <Grid
+                            item
+                            xs={2}
+                            sx={{ cursor: 'pointer' }}
+                            onClick={() => handleOpenServiceDialog(tableMeta.rowData)}
+                          >
+                            <MoreVertIcon sx={{ color: 'primary.main' }} />
                           </Grid>
-                        );
-                      },
+                        </Grid>
+                      );
                     },
                   },
-                  {
-                    name: 'team',
-                    label: t('teamLabel'),
-                    options: {
-                      filter: false,
-                      customBodyRender: (value, tableMeta, updateValue) => {
-                        return (
-                          <Grid container item alignItems={'center'}>
-                            <Grid item>
-                              <Typography>{value}</Typography>
-                            </Grid>
-                          </Grid>
-                        );
-                      },
-                    },
-                  },
-                ]
-          }
+                },
+            {
+              name: 'services',
+              label: t('servicesLabel'),
+              options: {
+                filter: false,
+                customBodyRender: (value, tableMeta, updateValue) => {
+                  return (
+                    <Grid container item minWidth={'150px'} maxWidth={'150px'} alignItems={'center'}>
+                      <Button fullWidth variant="contained" onClick={() => handleOpenServiceModal(tableMeta.rowData)}>
+                        {t('usersTable.VIEW ALL')}
+                      </Button>
+                    </Grid>
+                  );
+                },
+              },
+            },
+            {
+              name: 'team',
+              label: t('teamLabel'),
+              options: {
+                filter: false,
+                customBodyRender: (value, tableMeta, updateValue) => {
+                  return (
+                    <Grid container item alignItems={'center'}>
+                      <Grid item>
+                        <Typography>{value}</Typography>
+                      </Grid>
+                    </Grid>
+                  );
+                },
+              },
+            },
+          ]}
           options={options}
         />
       </Grid>
