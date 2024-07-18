5f386bd63f7754f840dd7c3ee48c030562a79c8d
Armada6199 <moh.abdin27@gmail.com>
2024-07-18 13:39:11 +0300

    sevicelist testing ,otp dialog validation

--- src/pages/services/tests/ServicesList.test.js
+++ src/pages/services/tests/ServicesList.test.js
@@ -1,17 +1,17 @@
 /* eslint-disable */
-import { render, screen } from "@testing-library/react";
-import Providers from "components/Providers";
-import { BrowserRouter } from "react-router-dom";
-import ServicesList from "../page/ServicesList";
-import ServicesListItem from "../page/utils/ServicesListItem";
-import { Services } from "../schema/ServicesSchema";
+import { render, screen } from '@testing-library/react';
+import Providers from 'components/Providers';
+import { BrowserRouter } from 'react-router-dom';
+import ServicesList from '../page/ServicesList';
+import ServicesListItem from '../page/utils/ServicesListItem';
+import { Services } from '../schema/ServicesSchema';
 
-describe("ServicesList Component", () => {
-  test("renders services for authorized user", () => {
+describe('ServicesList Component', () => {
+  test('renders services for authorized user', () => {
     const currentUserData = {
-      role: "MEMBER",
-      roboAuthorities: [{ name: "VISAS" }],
-      status: '1'
+      role: 'MEMBER',
+      roboAuthorities: [{ name: 'VISAS' }],
+      status: '1',
     };
 
     render(
@@ -19,12 +19,10 @@ describe("ServicesList Component", () => {
         <Providers>
           <ServicesList currentUserData={currentUserData} />
         </Providers>
-      </BrowserRouter>
+      </BrowserRouter>,
     );
     const services = Services.filter((service) =>
-      service.allowedAuthorities.some((auth) =>
-        currentUserData.roboAuthorities.map((a) => a.name).includes(auth)
-      )
+      service.allowedAuthorities.some((auth) => currentUserData.roboAuthorities.map((a) => a.name).includes(auth)),
     );
 
     // Check if all allowed services are rendered
@@ -33,78 +31,62 @@ describe("ServicesList Component", () => {
     });
   });
 
-  test("does  render services for unauthorized user", () => {
+  test('does  render services for unauthorized user loads skeleton', () => {
     const currentUserData = {
-      email:"S.ibrahim-c@takamol.com.sa",
-      firstName:"Samer",
-      lastName:"Ibrahim",
-      middleName:"Subhi",
-      roboAuthorities:[],
-      role:"MEMBER",
-      service:"VISAS",
-      status:"1 - User registered in QC",
-      team:"L2",
-      userId:173,
-      userName:"SamerUser"
-
+      email: 'S.ibrahim-c@takamol.com.sa',
+      firstName: 'Samer',
+      lastName: 'Ibrahim',
+      middleName: 'Subhi',
+      roboAuthorities: [],
+      role: 'MEMBER',
+      service: 'VISAS',
+      status: '1 - User registered in QC',
+      team: 'L2',
+      userId: 173,
+      userName: 'SamerUser',
     };
 
-    render(
+    const { container } = render(
       <BrowserRouter>
         <Providers>
-          <ServicesList currentUserDataParam={currentUserDataParam} />
+          <ServicesList currentUserData={currentUserData} />
         </Providers>
-      </BrowserRouter>
+      </BrowserRouter>,
     );
-    Services.forEach((service) => {
-      if (service.allowedAuthorities[0] !== "all" && currentUserData) {
-        expect(screen.queryByText(service.description))?.toBeInTheDocument();
-      }
-    });
+
+    expect(container.querySelector('.MuiSkeleton-root')).toBeTruthy();
   });
 });
 
-describe("ServicesListItem Component", () => {
+describe('ServicesListItem Component', () => {
   const service = Services[0]; // Example service
   const queryCenterSignup = false; // User has not signed up
 
-  test("renders service details correctly", () => {
+  test('renders service details correctly', () => {
     render(
       <BrowserRouter>
         <Providers>
-          <ServicesListItem
-            service={service}
-            queryCenterSignup={queryCenterSignup}
-          />
+          <ServicesListItem service={service} queryCenterSignup={queryCenterSignup} />
         </Providers>
         ,/
-      </BrowserRouter>
+      </BrowserRouter>,
     );
 
     // Check if the service name and image are displayed
     expect(screen.getByText(service.description)).toBeInTheDocument();
-    expect(screen.getByRole("img")).toHaveAttribute(
-      "src",
-      service.bcUrl
-    );
+    expect(screen.getByRole('img')).toHaveAttribute('src', service.bcUrl);
   });
 
-  test("click handler is attached correctly", () => {
+  test('click handler is attached correctly', () => {
     render(
       <BrowserRouter>
         <Providers>
-          <ServicesListItem
-            service={service}
-            queryCenterSignup={queryCenterSignup}
-          />
-        </Providers>{" "}
-      </BrowserRouter>
+          <ServicesListItem service={service} queryCenterSignup={queryCenterSignup} />
+        </Providers>{' '}
+      </BrowserRouter>,
     );
 
-    const serviceElement = screen.getByText(service.description).closest("a");
-    expect(serviceElement).toHaveAttribute(
-      "href",
-      `/dash/services/getAnswer/${service.service}`
-    );
+    const serviceElement = screen.getByText(service.description).closest('a');
+    expect(serviceElement).toHaveAttribute('href', `/dash/services/getAnswer/${service.service}`);
   });
 });
